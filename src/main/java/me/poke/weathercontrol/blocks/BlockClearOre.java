a58c54f35afb5fe292ab8790fe73f156afe123c5
pokeba <mypokeballzhurt@gmail.com>
2017-02-22 18:33:28 -0500

    Release 0.01

--- src/main/java/me/poke/weathercontrol/blocks/BlockClearOre.java
+++ src/main/java/me/poke/weathercontrol/blocks/BlockClearOre.java
@@ -4,6 +4,11 @@ import me.poke.weathercontrol.Reference;
 import me.poke.weathercontrol.WeatherControl;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class BlockClearOre extends Block {
 
