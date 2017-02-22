90028bf4e6b6149fd8f885973e01c05747e01dbd
pokeba <mypokeballzhurt@gmail.com>
2017-02-21 23:30:28 -0500

    Added potion effect

--- src/main/java/me/poke/weathercontrol/items/cores/ItemRainCore.java
+++ src/main/java/me/poke/weathercontrol/items/cores/ItemRainCore.java
@@ -23,6 +23,7 @@ public class ItemRainCore extends Item {
 	public void onUpdate(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
 		
 		EntityPlayer player = (EntityPlayer) entityIn;
+		player.addPotionEffect((new PotionEffect(Potion.getPotionById(2),600,1)));
 		//int timeUpdate = 200; //Every 10 sec
 		//if(worldIn.getWorldTime() % timeUpdate == 0)
 		//.setBlockState(entityIn.getPosition(), Blocks.FLOWING_WATER.getDefaultState());
